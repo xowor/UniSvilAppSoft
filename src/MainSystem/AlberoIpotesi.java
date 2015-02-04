db384fd3a31f1e46fdd6a1059fabe5f5a367dc93
jorkut <jorkut@Enterprise>
2015-02-04 10:31:33 +0100

    Modifica albero ipotesi

--- src/MainSystem/AlberoIpotesi.java
+++ src/MainSystem/AlberoIpotesi.java
@@ -10,32 +10,50 @@ import javax.swing.tree.TreeNode;
 public class AlberoIpotesi {
     
     private final JTree alberoIpotesi;
+    public int idCounter;
     
     public AlberoIpotesi(Messaggio cifrato){
         NodoIpotesi tmp = new NodoIpotesi(cifrato);
         alberoIpotesi = new JTree(tmp);
+        this.idCounter = 0;
     }
     
     /* Classe interna */
     private class NodoIpotesi implements TreeNode{
         
         private final int id;
-        private final NodoIpotesi parent;
+        private final int parent;
         private final Ipotesi ip;
         private ArrayList<NodoIpotesi> listaFigli = new ArrayList<>();
         
         private NodoIpotesi(Messaggio tmp){
             this.id = 0;
-            this.parent = null;
+            this.parent = 0;
             this.ip = new Ipotesi(tmp, this.id);
         }
         
-        private NodoIpotesi(Messaggio tmp, NodoIpotesi padre, int id){
+        private NodoIpotesi(Messaggio tmp, int padre, int id){
             this.id = id;
             this.parent = padre;
             this.ip = new Ipotesi(tmp, this.id);
         }
         
+        /* Aggiunge un nuovo figlio */
+        
+        public void addChild(Messaggio mex){
+            idCounter++;
+            this.listaFigli.add(new NodoIpotesi(mex, this.id, idCounter));        
+        }
+        
+        /* Ritorna l'id del padre */
+        
+        public int getIdParent() {
+            return this.parent;
+        }
+        
+        /* ****************************************************************** */
+        
+        /* Metodi in Override */
         
         @Override
         public TreeNode getChildAt(int childIndex) {
@@ -44,8 +62,7 @@ public class AlberoIpotesi {
 
         @Override
         public int getChildCount() {
-            
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            return this.listaFigli.size();
         }
 
         @Override
@@ -55,7 +72,7 @@ public class AlberoIpotesi {
 
         @Override
         public int getIndex(TreeNode node) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            return this.id;
         }
 
         @Override
@@ -65,7 +82,10 @@ public class AlberoIpotesi {
 
         @Override
         public boolean isLeaf() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            if(this.listaFigli.size() == 0)
+                return true;
+            else
+                return false;
         }
 
         @Override
@@ -73,5 +93,7 @@ public class AlberoIpotesi {
             throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         }
         
+        
+        /* ****************************************************************** */
     }
 }
