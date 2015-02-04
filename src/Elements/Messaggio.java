52d26ee3fbf3b8dc394c66b1d4d3814a8fa29e7a
Alessandro <Alessandro@Universe>
2015-02-04 19:42:08 +0100

    eliminato file doppio

--- src/Elements/Messaggio.java
+++ src/Elements/Messaggio.java
@@ -1,76 +1,86 @@
 package Elements;
 
 import java.util.List;
-import javax.management.Query;
 
 public class Messaggio {
-  public String id;
-  public String testo;
-  public String testoCifrato;
-  public String lingua;
-  public String titolo;
-  public boolean bozza;
-  public boolean letto;
-  public String idmitt;
-  public String iddest;
-  public String descmitt;
-  public String descdest;
+    public int id;
+    public String testo;
+    public String testoCifrato;
+    public String lingua;
+    public String oggetto;
+    public boolean bozza;
+    public boolean letto;
+    public int idMitt;
+    public int idDest;
+    public String descMitt;
+    public String descDest;
 
 
-  public Messaggio(String testo){
-      this.testo = testo;
-  }
+    public Messaggio(String testo){
+        this.testo = testo;
+    }
+    
+    public Messaggio(String titolo, String testo, int mitt, int dest){
+        this.oggetto = titolo;
+        this.testo = testo;
+        this.idMitt = mitt;
+        this.idDest = dest;
+    }
+    
+
+    public String getTesto(){
+        return this.testo;
+    }
+
+    public String getTestoCifrato(){
+        return this.testoCifrato;
+    }
+
+    public void setTesto(String testo){
+        this.testo = testo;
+    }
+
+    public void setTestoCifrato(String testoCifrato){
+        this.testoCifrato = testoCifrato ;
+    }
+    
+    public void setLetto(boolean b){
+        this.letto = b ;
+    }
+
+    public Messaggio load(String id){
+        return null;
+    }
+
+    public List<Messaggio> caricaBozze(Studente stud){
+
+        return null;
+    }
+
+    public List<Messaggio> caricaInviati(Studente stud){
+
+        return null;
+    }
+
+    public List<Messaggio> caricaRicevuti(Studente stud){
+
+        return null;
+    }
+
+    public boolean elimina(){
+
+        return false;
+    }
 
 
-  public String getTesto(){
-    return this.testo;
-  }
-  
-  public String getTestoCifrato(){
-    return this.testoCifrato;
-  }
-
-  public void setTesto(String testo){
-    this.testo = testo;
-  }
-  
-  public void setTestoCifrato(String testoCifrato){
-    this.testoCifrato = testoCifrato ;
-  }
-
-  public Messaggio load(String id){
-      return null;
-  }
-
-  public List<Messaggio> caricaBozze(Studente stud){
-      
-      return null;
-  }
-
-  public List<Messaggio> caricaInviati(Studente stud){
-      
-      return null;
-  }
-
-  public List<Messaggio> caricaRicevuti(Studente stud){
-      
-      return null;
-  }
-
-  public boolean elimina(){
-      
-      return false;
-  }
+    public boolean salva(){
+        return false;
+    }
 
 
-  public boolean salva(){
-      return false;
-  }
+    public void cifra(){
 
-
-  public void cifra(){
-
-  }
+    }
 
 
 }
