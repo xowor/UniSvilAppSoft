43b5720a104201593ca63829eada31db0bd745a5
jorkut <alegeno88@gmail.com>
2015-02-06 13:59:30 +0100

    SistemaDiCifratura

--- src/SistemaCifratura/InterfacciaCalcolatori.java
+++
@@ -1,6 +0,0 @@
-package SistemaCifratura;
-
-public interface InterfacciaCalcolatori {
-    public InterfacciaCalcolatori create(String met);
-    public Mappatura calcola(String chiave);
-}
--- src/SistemaCifratura/SistemaDiCifratura.java
+++ src/SistemaCifratura/SistemaDiCifratura.java
@@ -1,24 +1,49 @@
 package SistemaCifratura;
 
+import Elements.Studente;
+import MainSystem.DBManager;
+import java.util.ArrayList;
+
 
 public class SistemaDiCifratura {
-    public String id;
+    public int id;
     public String chiave;
     public String metodo;
+    CalcolatoreMappe calcola;
+    Mappatura mappatura;
 
 
     public SistemaDiCifratura(String key, String met){
         this.chiave = key;
         this.metodo = met;
+        this.calcola = CalcolatoreMappe.create(met);
+        salva();
+    }
+    
+    public SistemaDiCifratura(int id, String key, String met){
+        this.id = id;
+        this.chiave = key;
+        this.metodo = met;
+        this.calcola = CalcolatoreMappe.create(met);
+    }
+    
+    public ArrayList<SistemiDiCifratura> caricaSistemaCifratura(Studente s){
+        return DBManager.getSistemaDiCifratura(s);
     }
 
-
-    public Mappatura calcolaMappatura(String chiave, String metodo){
-        if(this.metodo.equals("cesare")){
-            return CalcolatoreCesare.cifraCesare(chiave);
-        } else if (this.metodo.equals("monoalfabetica")){
-            return null;
-        } else return null;
+    public SistemiDiCifratura caricaSistemaCifratura(int id){
+        return DBManager.getSistemaDiCifratura(id);
     }
-
+    
+    public void calcolaMappatura(){
+        this.mappatura = calcola.calcola(this.chiave);
+    }
+    
+    public boolean salva(){
+        return DBManager.salvaSistemaCifratura(this);
+    }
+    
+    public boolean elimina(){
+        return DBManager.eliminaSistemaCifratura(this.id);
+    }
 }
