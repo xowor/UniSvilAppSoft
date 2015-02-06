f0874c545d968abc617dec780ac6c507672c0a7d
Floralys <federica.granata28@gmail.com>
2015-02-06 14:30:26 +0100

    fixed

--- src/MainSystem/Sessione.java
+++ src/MainSystem/Sessione.java
@@ -14,7 +14,7 @@ public class Sessione {
     private Messaggio messaggioOriginaleCifrato;
     private AlberoIpotesi alberoIpotesi;
     private ArrayList<Supporto> strumentoSupporto;
-    private SistemaDiCifratura sistemaCifratura;
+    private ArrayList<SistemaDiCifratura> sistemaCifratura;
     
     // Sessione iniziale senza aver ancora deciso cosa decifrare
     public Sessione(int idStud, int idSes){
@@ -47,7 +47,7 @@ public class Sessione {
         this.alberoIpotesi = new AlberoIpotesi(this.idSessione, this.idAlbero, messaggioOriginaleCifrato.testoCifrato);
     }
     
-    public void setSistemaCifratura(int id){
+    public void setSistemaCifratura(Studente id){
         this.sistemaCifratura = DBManager.getSistemaDiCifratura(id);
     }
     
