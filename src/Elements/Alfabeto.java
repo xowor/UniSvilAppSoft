35e548b43291f616cf4c46b688d2f2776eca19a5
Floralys <federica.granata28@gmail.com>
2015-02-07 12:49:49 +0100

    aggiunto metodi

---
+++ src/Elements/Alfabeto.java
@@ -0,0 +1,29 @@
+package elements;
+
+import MainSystem.DBManager;
+import java.util.ArrayList;
+
+public class Alfabeto {
+    
+    private final String locale;
+    private ArrayList<String> lettere;
+    private Frequenze frequenze;
+    
+    public Alfabeto(String lingua){
+        this.locale = lingua;
+        this.lettere = DBManager.getAlfabeto(lingua);
+        this.frequenze = new Frequenze(lingua);
+    }
+    
+    public String getLocale(){
+        return this.locale;
+    }
+    
+    public ArrayList<String> getLettere(){
+        return this.lettere;
+    }
+    
+    public Frequenze getFrequenze(){
+        return this.frequenze;
+    }
+}
